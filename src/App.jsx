7f73565221bdf4dd04b95e9228489dea2b569465
tonyopuodho <tonyopuodho@gmail.com>
2026-05-25 16:26:44 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl text-red-800 font-sans">Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
