c1359dc83b72f769b1366fb0fa4e072eefdf3460
tonyopuodho <tonyopuodho@gmail.com>
2026-05-25 16:41:49 +0300

    Adding different routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/home/Home"
+import Programm from "./pages/program/Programm"
+import Gallery from "./pages/gallery/Gallery"
+import Contact from "./pages/contact/Contact"
+import About from "./pages/About/About"
 
 
 function App() {
  
   return (
     <>
-      <div>
-        <h1 className="text-2xl text-red-800 font-sans">Hello world</h1>
-      </div>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/program" element={<Programm/>}/>
+      <Route path="/gallery" element={<Gallery/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/about" element={<About/>}/>
+     </Routes>
     </>
   )
 }
